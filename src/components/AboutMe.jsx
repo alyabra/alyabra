ad1a29cbbc2b914f0122d1df5c33b2b59d0e2b18
a-horse-with-no-name <alyabra.vargas@gmail.com>
2023-02-21 17:10:51 -0600

    Imagenes obtenidas desde local

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './AboutMe.css'
-// import photo from '../assets/photo_2022-10-19_11-37-00.jpg'
+import photo from '/src/assets/images/photo_2022-10-19_11-37-00_pzes6q.jpg'
 import { useState } from 'react'
-import ListAboutMe from './ListAboutMe'
-import AboutMeDescription from './AboutMeDescription'
 import AboutMeBook from './AboutMeBook'
 
 
@@ -12,7 +10,6 @@ const AboutMe = () => {
   const change = (stateOn) => {
       setState(stateOn)
   }  
-  const photo = 'https://res.cloudinary.com/dxslrgshk/image/upload/v1674931329/briefcase/photo_2022-10-19_11-37-00_pzes6q.jpg'
   return (
     <section className='aboutMe-container' id='aboutMe'>
         <h2 className='section-title aboutMe-info--container-title'>Sobre mi</h2>
