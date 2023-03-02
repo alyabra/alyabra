e127627ed067a434ae42464b36896ca576257a36
a-horse-with-no-name <alyabra.vargas@gmail.com>
2023-03-02 09:54:41 -0600

    Fix load picture photo

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './AboutMe.css'
 // import photo from '/src/assets/images/photo_2022-10-19_11-37-00_pzes6q.jpg'
-import photo from 'https://res.cloudinary.com/dxslrgshk/image/upload/v1674931329/briefcase/photo_2022-10-19_11-37-00_pzes6q.jpg'
+// import photo from 'https://res.cloudinary.com/dxslrgshk/image/upload/v1674931329/briefcase/photo_2022-10-19_11-37-00_pzes6q.jpg'
+// https://res.cloudinary.com/dxslrgshk/image/upload/v1674931329/briefcase/photo_2022-10-19_11-37-00_pzes6q.jpg
+
 import AboutMeBook from '../components/AboutMeBook'
 import { useEffect } from 'react'
 useEffect
 
-const AboutMe = ({myref}) => { 
+const AboutMe = ({ myref }) => { 
+  const photo = 'https://res.cloudinary.com/dxslrgshk/image/upload/v1674931329/briefcase/photo_2022-10-19_11-37-00_pzes6q.jpg'
   return (
     <section className='aboutMe-container' id='aboutMe' ref={myref} >
         <h2 className='section-title aboutMe-info--container-title'>Sobre mi</h2>
